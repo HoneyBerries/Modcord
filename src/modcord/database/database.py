1db50c52829756ef11b6f433774f67ebf7d3be97
HoneyBerries <henry.rainbowfish@gmail.com>
2025-11-23 17:03:31 -0800

    Simplify the database class a bit

--- src/modcord/database/database.py
+++ src/modcord/database/database.py
@@ -288,17 +288,7 @@ class Database:
 
 
 # Global database instance
-_db_instance: Database | None = None
-
+_db_instance = Database()
 
 def get_db() -> Database:
-    """
-    Get the global database instance.
-    
-    Returns:
-        Database: The global Database instance.
-    """
-    global _db_instance
-    if _db_instance is None:
-        _db_instance = Database()
     return _db_instance
